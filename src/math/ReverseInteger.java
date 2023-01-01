c837746492fb29beb057f6dfc1ac37c5304815aa
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-01-01 22:43:31 +0530

    reverse integer correct answer

--- src/math/ReverseInteger.java
+++ src/math/ReverseInteger.java
@@ -9,13 +9,26 @@ import java.util.Scanner;
 
 public class ReverseInteger {
     public static int reverse(int x) {
-        String str = Integer.toString(x);
-        StringBuffer sb = new StringBuffer(str);
-        String s = String.valueOf(sb.reverse());
-        int num = Integer.parseInt(s);
-        return num;
-    }
+//        String str = Integer.toString(x);
+//        StringBuffer sb = new StringBuffer(str);
+//        String s = String.valueOf(sb.reverse());
+//        int num = Integer.parseInt(s);
+//        return num;
+        int temp;
+        long reverse = 0;
 
+        while (x != 0) {
+            temp = x % 10;
+            reverse = reverse * 10 + temp;
+            x = x / 10;
+        }
+        if (reverse >= Integer.MIN_VALUE && reverse <= Integer.MAX_VALUE)
+            return (int) reverse;
+        else
+            return 0;
+
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
