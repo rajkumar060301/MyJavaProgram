20a523c53cc887f51435cd3c35bb1059c3ded3a1
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2025-01-16 12:42:00 +0530

    all files commit

---
+++ src/converting/StringToInteger.java
@@ -0,0 +1,15 @@
+package converting;
+
+public class StringToInteger {
+
+    public static void main(String[] args) {
+        
+        // Converting string to integer
+        String str= "45";
+        int number = Integer.parseInt(str);
+
+        System.out.println(number);
+        
+
+    }
+}
